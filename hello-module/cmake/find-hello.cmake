48a7c605b4b973baa7aa75c0a9acac72e874dd36
Akagi201 <akagi201@gmail.com>
2014-10-17 16:43:49 +0800

    add hello-module

---
+++ hello-module/cmake/find-hello.cmake
@@ -0,0 +1,17 @@
+find_path(HELLO_INCLUDE_DIR hello.h /usr/include/hello /usr/local/include/hello)
+find_library(HELLO_LIBRARY NAMES hello PATH /usr/lib /usr/local/lib)
+
+if(HELLO_INCLUDE_DIR AND HELLO_LIBRARY)
+  SET(HELLO_FOUND true)
+endif(HELLO_INCLUDE_DIR AND HELLO_LIBRARY)
+
+if(HELLO_FOUND)
+  if(NOT HELLO_FIND_QUIETLY)
+    message(STATUS "Found Hello: ${HELLO_INCLUDE_DIR} ${HELLO_LIBRARY}")
+  endif(NOT HELLO_FIND_QUIETLY)
+else(HELLO_FOUND)
+  if(HELLO_FIND_REQUIRED)
+    message(FATAL_ERROR "Could not find hello library")
+  endif(HELLO_FIND_REQUIRED)
+endif(HELLO_FOUND)
+
---
+++ hello-module/src/main.c
@@ -0,0 +1,7 @@
+#include <hello.h>
+
+int main() {
+  HelloFunc();
+  return 0;
+}
+
